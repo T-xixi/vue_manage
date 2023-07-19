e075f882ffef6c3990de23909c6e7b3c5bd558ca
西西早早 <973717307@qq.com>
2023-07-19 20:08:33 +0800

    modified

--- vue.config.js
+++ vue.config.js
@@ -1,34 +1,34 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
 
-// })
-module.exports = {
-  chainWebpack: config => {
-    // 发布模式
-    config.when(process.env.NODE_ENV === 'production', config => {
-      config.entry('app').clear().add('./src/main-prod.js')
-      config.set('externals', {
-        vue: 'Vue',
-        'vue-router': 'VueRouter',
-        axios: 'axios',
-        lodash: '_',
-        echarts: 'echarts',
-        nprogress: 'NProgress',
-        'vue-quill-editor': 'VueQuillEditor'
-      })
-      config.plugin('html').tap(args => {
-        args[0].isProd = true
-        return args
-      })
-    })
-    // 打包模式
-    config.when(process.env.NODE_ENV === 'development', config => {
-      config.entry('app').clear().add('./src/main-dev.js')
-      config.plugin('html').tap(args => {
-        args[0].isProd = false
-        return args
-      })
-    })
-  }
-}
+})
+// module.exports = {
+//   chainWebpack: config => {
+//     // 发布模式
+//     config.when(process.env.NODE_ENV === 'production', config => {
+//       config.entry('app').clear().add('./src/main-prod.js')
+//       config.set('externals', {
+//         vue: 'Vue',
+//         'vue-router': 'VueRouter',
+//         axios: 'axios',
+//         lodash: '_',
+//         echarts: 'echarts',
+//         nprogress: 'NProgress',
+//         'vue-quill-editor': 'VueQuillEditor'
+//       })
+//       config.plugin('html').tap(args => {
+//         args[0].isProd = true
+//         return args
+//       })
+//     })
+//     // 打包模式
+//     config.when(process.env.NODE_ENV === 'development', config => {
+//       config.entry('app').clear().add('./src/main-dev.js')
+//       config.plugin('html').tap(args => {
+//         args[0].isProd = false
+//         return args
+//       })
+//     })
+//   }
+// }
